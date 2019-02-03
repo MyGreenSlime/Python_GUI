bc85307bd4f0b0cf18865a839c36527843a06f9d
don <ruksachai@gmail.com>
2019-02-04 01:57:03 +0700

    test radiobox

--- gui_1.py
+++ gui_1.py
@@ -40,5 +40,27 @@ check3 = tk.Checkbutton(win, text = 'Enabled', variable = chVarEn)
 check3.select()
 check3.grid(column = 2, row = 4, sticky = tk.W)
 
+COLOR1 = 'Blue'
+COLOR2 = 'Yellow'
+COLOR3 = 'Red'
+
+def radCall():
+    radSel = radVar.get()
+    if radSel == 1:
+        win.configure(background = COLOR1)
+    elif radSel == 2:
+        win.configure(background = COLOR2)
+    else :
+        win.configure(background = COLOR3)
+
+radVar = tk.IntVar()
+rad1 = tk.Radiobutton(win, text=COLOR1, variable = radVar, value=1, command = radCall)
+rad1.grid(column = 0, row = 5,sticky = tk.W, columnspan = 3)
+rad2 = tk.Radiobutton(win, text=COLOR2, variable = radVar, value=2, command = radCall)
+rad2.grid(column = 1, row = 5,sticky = tk.W, columnspan = 3)
+rad3 = tk.Radiobutton(win, text=COLOR3, variable = radVar, value=3, command = radCall)
+rad3.grid(column = 2, row = 5,sticky = tk.W, columnspan = 3)
+
+
 
 win.mainloop()
