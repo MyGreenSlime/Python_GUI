9b5b306d57a902f6eb1510a9ef568e5b3f9154c8
don <ruksachai@gmail.com>
2019-02-03 19:24:10 +0700

    test button

--- gui_1.py
+++ gui_1.py
@@ -4,6 +4,14 @@ win = tk.Tk()
 
 win.title('Python GUI')
 
-ttk.Label(win, text="A Label").grid(column = 0, row = 0)
+aLabel = ttk.Label(win, text="A Label")
+aLabel.grid(column = 0, row = 0)
+
+def ClickMe():
+    action.configure(text=" I have been clicked")
+    aLabel.configure(foreground = 'red')
+    aLabel.configure(text = "Blue")
+action = ttk.Button(win, text="Click Me!", command = ClickMe)
+action.grid(column = 1, row = 0)
 
 win.mainloop()
