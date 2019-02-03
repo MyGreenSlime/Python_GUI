95f6aa328887dec74c9ed23cbf2fe159f888afba
don <ruksachai@gmail.com>
2019-02-03 21:49:48 +0700

    test checkbox

--- gui_1.py
+++ gui_1.py
@@ -8,7 +8,7 @@ aLabel = ttk.Label(win, text ="Enter Your name")
 aLabel.grid(column = 0, row = 0)
 
 def ClickMe():
-    action.configure(text ="Hello"+name.get())
+    action.configure(text ="Hello"+name.get()+" "+number.get())
 action = ttk.Button(win, text="Click Me!", command = ClickMe)
 action.grid(column = 2, row = 1)
 
@@ -24,6 +24,21 @@ number_chosen.grid(column = 1, row = 1)
 number_chosen.current(0)
 
 name_entered.focus() #place cursor into text box
+
+chVarDis = tk.IntVar()
+check1 = tk.Checkbutton(win, text = 'Disabled', variable = chVarDis, state = 'disabled')
+check1.select()
+check1.grid(column = 0, row = 4, sticky = tk.W)
+
+chVarUn = tk.IntVar()
+check2 = tk.Checkbutton(win, text = 'Unchecked', variable = chVarUn)
+check2.deselect()
+check2.grid(column = 1, row = 4, sticky = tk.W)
+
+chVarEn = tk.IntVar()
+check3 = tk.Checkbutton(win, text = 'Enabled', variable = chVarEn)
+check3.select()
+check3.grid(column = 2, row = 4, sticky = tk.W)
 
 
 win.mainloop()
